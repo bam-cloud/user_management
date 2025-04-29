1962be61a845f221ce18882d4b29934449842a73
bam-cloud <bam96@njit.edu>
2025-04-29 11:42:08 -0500

    fixed obsolete property

--- app/schemas/token_schema.py
+++ app/schemas/token_schema.py
@@ -6,7 +6,7 @@ class TokenResponse(BaseModel):
     token_type: str = "bearer"
 
     class Config:
-        json_schema_extra = {
+        schema_extra = {
             "example": {
                 "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lQGV4YW1wbGUuY29tIiwicm9sZSI6IkFVVEhFTlRJQ0FURUQiLCJleHAiOjE2MjQzMzQ5ODR9.ZGNjNjI2ZjI4MmYzNTk0MjVjNDk0ZjI4MjdjNGEzNmI1",
                 "token_type": "bearer"
