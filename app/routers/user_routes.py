0083e7a0fe7decb18de2492f3dc46b69e0cb9503
bam-cloud <bam96@njit.edu>
2025-04-29 12:54:25 -0500

    fixed the issue by changing the tokenurl from login to token

--- app/routers/user_routes.py
+++ app/routers/user_routes.py
@@ -33,8 +33,27 @@ from app.services.jwt_service import create_access_token
 from app.utils.link_generation import create_user_links, generate_pagination_links
 from app.dependencies import get_settings
 from app.services.email_service import EmailService
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 router = APIRouter()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+@router.post("/token", response_model=TokenResponse)
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), session: AsyncSession = Depends(get_db)):
+     if await UserService.is_account_locked(session, form_data.username):
+         raise HTTPException(status_code=400, detail="Account locked due to too many failed login attempts.")
+ 
+     user = await UserService.login_user(session, form_data.username, form_data.password)
+     if user:
+         access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+ 
+         access_token = create_access_token(
+             data={"sub": user.email, "role": str(user.role.name)},
+             expires_delta=access_token_expires
+         )
+ 
+         return {"access_token": access_token, "token_type": "bearer"}
+     raise HTTPException(status_code=401, detail="Incorrect email or password.")
+ 
+ 
 settings = get_settings()
 @router.get("/users/{user_id}", response_model=UserResponse, name="get_user", tags=["User Management Requires (Admin or Manager Roles)"])
 async def get_user(user_id: UUID, request: Request, db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme), current_user: dict = Depends(require_role(["ADMIN", "MANAGER"]))):
